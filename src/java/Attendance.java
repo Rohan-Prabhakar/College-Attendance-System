0db50f8d38cff3faedc5b78cc51312aac2ff211e
Mridulvij289 <75908327+Mridulvij289@users.noreply.github.com>
2021-04-19 19:10:51 +0530

    Update Attendance.java

--- src/java/Attendance.java
+++ src/java/Attendance.java
@@ -14,9 +14,10 @@ import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+
 
 /**
  *
